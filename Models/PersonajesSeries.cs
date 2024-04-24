2a5c955dd5bdec4563f17c8b1dce0a266495b8cd
alejandropj <alejandroparrajimenez@gmail.com>
2024-04-24 11:52:14 +0200

    Add project files.

---
+++ Models/PersonajesSeries.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MvcPracticaPersonajes.Models
+{
+    public class PersonajesSeries
+    {
+        public int IdPersonaje { get; set; }
+        public string Nombre { get; set; }
+        public string Imagen { get; set; }
+        public string Serie { get; set; }
+    }
+}
---
+++ Services/ServiceApiPersonajes.cs
@@ -0,0 +1,38 @@
+﻿using System.Net.Http.Headers;
+
+namespace MvcPracticaPersonajes.Services
+{
+    public class ServiceApiPersonajes
+    {
+        private string UrlApi;
+        private MediaTypeWithQualityHeaderValue header;
+
+        public ServiceApiPersonajes(IConfiguration config)
+        {
+            this.header = new MediaTypeWithQualityHeaderValue("application/json");
+            this.UrlApi = config.GetValue<string>
+                ("ApiUrls:ApiPersonajes");
+        }
+
+        private async Task<T> CallApiAsync<T>(string request)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(this.UrlApi);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.header);
+                HttpResponseMessage response = await client.GetAsync(request);
+                if (response.IsSuccessStatusCode)
+                {
+                    T data = await response.Content.ReadAsAsync<T>();
+                    return data;
+                }
+                else
+                {
+                    return default(T);
+                }
+            }
+        }
+
+    }
+}
