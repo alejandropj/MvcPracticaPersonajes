675b550ca1f798644567e140cf5781908539b792
alejandropj <alejandroparrajimenez@gmail.com>
2024-04-24 13:06:20 +0200

    Final release

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
+using MvcPracticaPersonajes.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddTransient<ServiceApiPersonajes>();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
