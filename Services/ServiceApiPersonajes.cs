565a6fd521fe1e3d80d162df214386ab41e1bd0c
alejandropj <alejandroparrajimenez@gmail.com>
2024-04-24 11:56:05 +0200

    addition of services

--- Services/ServiceApiPersonajes.cs
+++ Services/ServiceApiPersonajes.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Http.Headers;
+﻿using MvcPracticaPersonajes.Models;
+using System.Net.Http.Headers;
 
 namespace MvcPracticaPersonajes.Services
 {
@@ -32,6 +33,39 @@ namespace MvcPracticaPersonajes.Services
                     return default(T);
                 }
             }
+        }
+
+        public async Task<List<PersonajesSeries>>
+            GetPersonajesSeriesAsync()
+        {
+            string request = "api/personajes";
+            List<PersonajesSeries> data = await this.CallApiAsync
+                <List<PersonajesSeries>>(request);
+            return data;
+        }           
+        public async Task<List<string>>
+            GetSeriesAsync()
+        {
+            string request = "api/personajes/series";
+            List<string> data = await this.CallApiAsync
+                <List<string>>(request);
+            return data;
+        }        
+        public async Task<PersonajesSeries>
+            FindPersonajeSerieAsync(int id)
+        {
+            string request = "api/personajes/"+id;
+            PersonajesSeries data = await this.CallApiAsync
+                <PersonajesSeries>(request);
+            return data;
+        }        
+        public async Task<List<PersonajesSeries>>
+            FindPersonajesBySerieAsync(string serie)
+        {
+            string request = "api/personajes/personajesseries/"+serie;
+            List<PersonajesSeries> data = await this.CallApiAsync
+                <List<PersonajesSeries>>(request);
+            return data;
         }
 
     }
